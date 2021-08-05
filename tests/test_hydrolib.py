7b78449721c3c9765bea77494e741d8aca7336b6
Maarten Pronk <git@evetion.nl>
2021-08-05 10:21:32 +0200

    Update lock and test.

--- tests/test_hydrolib.py
+++ tests/test_hydrolib.py
@@ -3,3 +3,10 @@ from hydrolib.post import __version__
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_namespace():
+    # Make sure we can access hydrolib-core package
+    from hydrolib.core import __version__
+
+    assert __version__ == "0.1.0"
