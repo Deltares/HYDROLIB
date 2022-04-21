3c7b6667c6966f0649a38761b674c01b631a34ae
grongen <guusrongen@gmail.com>
2022-04-21 16:31:34 +0200

    Update test_hydrolib.py
    Update version from 0.1.0 to 0.1.5

--- tests/test_hydrolib.py
+++ tests/test_hydrolib.py
@@ -2,11 +2,11 @@ from hydrolib.post import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.5"
 
 
 def test_namespace():
     # Make sure we can access hydrolib-core package
     from hydrolib.core import __version__
 
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.5
