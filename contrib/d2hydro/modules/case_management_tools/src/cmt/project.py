869b2e3e90d1358afcb0d5d69b6d8b63f3cc16c7
d2hydro <d2hydro@users.noreply.github.com>
2022-04-22 07:50:15 +0000

    autoformat: isort & black

--- contrib/d2hydro/modules/case_management_tools/src/cmt/project.py
+++ contrib/d2hydro/modules/case_management_tools/src/cmt/project.py
@@ -1,22 +1,24 @@
-from pydantic import BaseModel
-from typing import List, Optional, Union
+import logging
+import shutil
+from datetime import datetime, timedelta
+from multiprocessing.pool import ThreadPool
 from pathlib import Path
-from cmt.utils.readers import read_stochastics, read_text
-from cmt.utils.writers import (
-    write_stowa_buien,
-    write_flow_boundaries,
-    write_models,
-    write_rr_conditions
-    )
+from typing import List, Optional, Union
+
 from cmt.run import dhydro
 from cmt.utils.modifyers import prefix_to_paths
-from datetime import datetime, timedelta
-from hydrolib.core.io.mdu.models import FMModel
-from hydrolib.core.io.fnm.models import RainfallRunoffModel
-import shutil
-import logging
-from multiprocessing.pool import ThreadPool
+from cmt.utils.readers import read_stochastics, read_text
+from cmt.utils.writers import (
+    write_flow_boundaries,
+    write_models,
+    write_rr_conditions,
+    write_stowa_buien,
+)
+from pydantic import BaseModel
 from pydantic.types import DirectoryPath
+
+from hydrolib.core.io.fnm.models import RainfallRunoffModel
+from hydrolib.core.io.mdu.models import FMModel
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -100,9 +102,7 @@ class Project(BaseModel):
         return next((i for i in self.models if i.id == model_id))
 
     def run_cases(self, case_ids, workers=None):
-        logger.info(
-            f"Start running batch: #cases: {len(case_ids)}, #workers {workers}"
-            )
+        logger.info(f"Start running batch: #cases: {len(case_ids)}, #workers {workers}")
         tp = ThreadPool(workers)
         for idx, case_id in enumerate(case_ids):
             progress = f" ({idx+1}/{len(case_ids)})"
@@ -128,14 +128,15 @@ class Project(BaseModel):
                 case.run.completed = True
                 case.run.success = True
                 run_duration = datetime.now() - start_datetime
-                case.run.run_duration = run_duration 
-                logger.info(f"finished running case: '{case_id}' in {run_duration.total_seconds()} secs")
+                case.run.run_duration = run_duration
+                logger.info(
+                    f"finished running case: '{case_id}' in {run_duration.total_seconds()} secs"
+                )
             else:
                 case.run.completed = False
                 case.run.success = False
                 logger.error(f"Case: '{case_id}' crashed")
             self.write_manifest()
-            
 
     def get_flow_boundary(self, bc_id):
         return next((i for i in self.boundary_conditions.flow if i.id == bc_id))
@@ -146,13 +147,15 @@ class Project(BaseModel):
     def write_manifest(self):
         index_json = self.filepath / "manifest.json"
 
-        index_json.write_text(self.json(
-            exclude={"filepath"},
-            indent=1,
-            ))
+        index_json.write_text(
+            self.json(
+                exclude={"filepath"},
+                indent=1,
+            )
+        )
 
     @classmethod
-    def from_manifest(cls, manifest_json:  Union[str, Path]):
+    def from_manifest(cls, manifest_json: Union[str, Path]):
         cls = cls.parse_raw(read_text(manifest_json))
         cls.filepath = Path(manifest_json).parent.absolute().resolve()
         return cls
@@ -160,7 +163,7 @@ class Project(BaseModel):
     def from_stochastics(self, stochastics_json: Path):
         self._init_filepath()
         stochastics_dict = read_stochastics(stochastics_json)
-        src_dir = stochastics_json.parent 
+        src_dir = stochastics_json.parent
         # do boundaries
         if "boundary_conditions" in stochastics_dict.keys():
             boundaries = stochastics_dict["boundary_conditions"]
@@ -169,9 +172,7 @@ class Project(BaseModel):
             if "meteo" in boundaries.keys():
                 events = boundaries["meteo"]
                 meteo_bc_path = self._create_subdir(r"boundary_conditions/meteo")
-                events = [
-                    i for i in events if "stowa_bui" in i.keys()
-                    ]
+                events = [i for i in events if "stowa_bui" in i.keys()]
                 bcs = write_stowa_buien(meteo_bc_path, events)
                 self.boundary_conditions.meteo = [BoundaryCondition(**i) for i in bcs]
 
@@ -191,7 +192,7 @@ class Project(BaseModel):
                 write_rr_conditions(rr_ini_path, conditions, src_dir)
                 self.initial_conditions.rr = [
                     InitialCondition(id=i["id"], name=i["name"]) for i in conditions
-                    ]
+                ]
 
         # do models
         if "models" in stochastics_dict.keys():
@@ -199,22 +200,20 @@ class Project(BaseModel):
             models = stochastics_dict["models"]
             write_models(models_path, models, src_dir)
             self.models = [
-                Model(id=i["id"],
-                      name=i["name"],
-                      mdu=i["mdu_file"],
-                      fnm=i["fnm_file"]) for i in models
-                ]
+                Model(id=i["id"], name=i["name"], mdu=i["mdu_file"], fnm=i["fnm_file"])
+                for i in models
+            ]
 
         if "cases" in stochastics_dict.keys():
             cases_path = self._create_subdir(r"cases")
             cases = stochastics_dict["cases"]
             for model_id in set([i["model_id"] for i in cases]):
                 model = self.get_model(model_id)
-                cases_subset = [i for i in cases if i["model_id"] == model_id] 
+                cases_subset = [i for i in cases if i["model_id"] == model_id]
                 mdu = FMModel(filepath=self.filepath / "models" / model.id / model.mdu)
                 mdu_parent = mdu.filepath.parent
                 mdu.general.pathsrelativetoparent = True
-                mdu.time.tunit="S"
+                mdu.time.tunit = "S"
                 dimr_path = self.filepath / "models" / model.id / "dimr_config.xml"
                 rtc_dir = self.filepath / "models" / model.id / "rtc"
                 for section in ["geometry", "restart", "output"]:
@@ -222,56 +221,86 @@ class Project(BaseModel):
                     for i in mdu_section.__fields__.keys():
                         if not i in ["outputdir"]:
                             if hasattr(mdu_section, i):
-                                setattr(mdu_section,
-                                        i,
-                                        prefix_to_paths(getattr(mdu_section, i),
-                                                        prefix=f"../../../models/{model_id}/{mdu_parent.name}",))
+                                setattr(
+                                    mdu_section,
+                                    i,
+                                    prefix_to_paths(
+                                        getattr(mdu_section, i),
+                                        prefix=f"../../../models/{model_id}/{mdu_parent.name}",
+                                    ),
+                                )
                 ext = mdu.external_forcing.extforcefilenew
                 ext_path = str(ext.filepath)
-                fnm = RainfallRunoffModel(filepath=self.filepath / "models" / model.id / model.fnm)
+                fnm = RainfallRunoffModel(
+                    filepath=self.filepath / "models" / model.id / model.fnm
+                )
                 fnm_parent = fnm.filepath.parent
                 fnm_prefix = f"../../../models/{model_id}/{fnm_parent.name}"
-                exclude_suffices = [".log", ".out", ".dbg", ".his", ".abr", ".msg", ".fnm"]
+                exclude_suffices = [
+                    ".log",
+                    ".out",
+                    ".dbg",
+                    ".his",
+                    ".abr",
+                    ".msg",
+                    ".fnm",
+                ]
                 exclude_names = ["sobek3b_progress.txt", "RSRR_OUT", "RR-ready"]
                 for i in fnm.__fields__.keys():
-                    setattr(fnm, i, prefix_to_paths(getattr(fnm, i),
-                                    prefix=fnm_prefix,
-                                    exclude_suffices=exclude_suffices,
-                                    exclude_names=exclude_names))
+                    setattr(
+                        fnm,
+                        i,
+                        prefix_to_paths(
+                            getattr(fnm, i),
+                            prefix=fnm_prefix,
+                            exclude_suffices=exclude_suffices,
+                            exclude_names=exclude_names,
+                        ),
+                    )
                 for i in cases_subset:
                     print(rf"{i['id']}")
 
                     # read start_datetime
                     # set paths
                     flow_bc = self.get_flow_boundary(i["flow_bc_id"])
-                    flow_bc_path = Path(f"../../../boundary_conditions/flow/{flow_bc.id}.bc")
+                    flow_bc_path = Path(
+                        f"../../../boundary_conditions/flow/{flow_bc.id}.bc"
+                    )
                     dst_path = cases_path / rf"{i['id']}"
                     for bc in ext.boundary:
                         bc.forcingfile.filepath = flow_bc_path
 
                     meteo_bc = self.get_meteo_boundary(i["meteo_bc_id"])
-                    bui_path = Path(f"../../../boundary_conditions/meteo/{meteo_bc.id}.bui")
-                    evp_path = Path(f"../../../boundary_conditions/meteo/{meteo_bc.id}.evp")
-                    
-                    start_datetime = max(flow_bc.start_datetime, meteo_bc.start_datetime)
-                    end_datetime = min(flow_bc.start_datetime + flow_bc.timedelta,
-                                   meteo_bc.start_datetime + meteo_bc.timedelta)
-                    time_delta = end_datetime - start_datetime
+                    bui_path = Path(
+                        f"../../../boundary_conditions/meteo/{meteo_bc.id}.bui"
+                    )
+                    evp_path = Path(
+                        f"../../../boundary_conditions/meteo/{meteo_bc.id}.evp"
+                    )
 
+                    start_datetime = max(
+                        flow_bc.start_datetime, meteo_bc.start_datetime
+                    )
+                    end_datetime = min(
+                        flow_bc.start_datetime + flow_bc.timedelta,
+                        meteo_bc.start_datetime + meteo_bc.timedelta,
+                    )
+                    time_delta = end_datetime - start_datetime
 
                     # write flow model
                     mdu_path = dst_path / model.mdu
                     ext.save(mdu_path.parent / ext_path)
-                    ext.filepath.write_text(ext.filepath.read_text().replace("locationFile =\n",""))
+                    ext.filepath.write_text(
+                        ext.filepath.read_text().replace("locationFile =\n", "")
+                    )
                     ext.filepath = ext_path
                     mdu.time.refdate = int(start_datetime.strftime("%Y%m%d"))
                     time = start_datetime.time()
-                    mdu.time.tstart = timedelta(hours=time.hour,
-                                                minutes=time.minute,
-                                                seconds=time.second).total_seconds()
+                    mdu.time.tstart = timedelta(
+                        hours=time.hour, minutes=time.minute, seconds=time.second
+                    ).total_seconds()
                     mdu.time.tstop = time_delta.total_seconds()
                     mdu.save(dst_path / model.mdu)
-                    
 
                     # copy rtc model
                     shutil.copytree(rtc_dir, dst_path / "rtc")
@@ -283,21 +312,23 @@ class Project(BaseModel):
 
                     # write dimr
                     shutil.copy(dimr_path, dst_path / "dimr_config.xml")
-                    self.cases +=[
-                        Case(id=i["id"],
-                             name=i["name"],
-                             meteo_bc_id=i["meteo_bc_id"],
-                             flow_bc_id=i["flow_bc_id"],
-                             model_id=i["model_id"],
-                             start_datetime=start_datetime,
-                             simulation_period=time_delta)
-                        ]
-                    
+                    self.cases += [
+                        Case(
+                            id=i["id"],
+                            name=i["name"],
+                            meteo_bc_id=i["meteo_bc_id"],
+                            flow_bc_id=i["flow_bc_id"],
+                            model_id=i["model_id"],
+                            start_datetime=start_datetime,
+                            simulation_period=time_delta,
+                        )
+                    ]
+
                     # write run.bat
                     bat_path = self.filepath / "models" / model.id / "run.bat"
                     if bat_path.exists():
                         shutil.copy(bat_path, dst_path / "run.bat")
 
-        self.write_manifest()  
+        self.write_manifest()
 
         return self
