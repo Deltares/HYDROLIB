50b56dedbf830b58b29590575701f35d7adb72ed
Kreef <31723681+Kreef@users.noreply.github.com>
2022-05-13 16:01:12 +0200

    Add files via upload

--- contrib/HydroLogic/hisreader.py
+++ contrib/HydroLogic/hisreader.py
@@ -2,31 +2,32 @@
 """
 Created on Tue Dec 14 10:53:59 2021
 
-@author: Krijn Prins Hydrologic
 @author: Koen Reef Hydrologic
-
-Update with variables, observation station names, weir names, and writing weir_data
 """
-
-import datetime as dt
 
 # =============================================================================
 # Import
 # =============================================================================
-import os
-import pathlib
-from datetime import datetime
-
+import datetime as dt
+import matplotlib.pyplot as plt
 import netCDF4 as nc
 import numpy as np
+import os
 import pandas as pd
+import pathlib
 import ugfile as uf
+from datetime import datetime
+from hydrolib.core.io.structure.models import Structure
+from pydantic import BaseModel, Field
+from typing import Optional, Literal, List, Type, TypeVar
+
+PandasDataFrame = TypeVar("pandas.core.frame.DataFrame")
 
 """============================================================================
 Provides
     1. Creates an object to convert variables from the his netcdf output files
-    from the D-Hydro model (...his.nc) into .csv file format
-    Use: hisreader(inputdir, outputdir)
+    from the D-Hydro model (...his.nc) into python objects
+    Use: HisResults(inputdir, outputdir)
         Parameters
         ----------
         inputdir : absolute windows directory path to the ...his.nc D-Hydro output file
@@ -35,207 +36,163 @@ Provides
 
         Returns
         -------
-        Hisreader object with supported functions to extract variables:
-            observation stations          writeobsstations()
-            cross sections                writecrosssections()
-            dambreaks                     writedambreaks()
-            all of variables above        writeall()
-        To create the csv file with the extracted variables use:
-                                          writehisdata()
+        HisResults object with lists of objects retrieved from his file
 ==========================================================================="""
+# TODO: create function in hisreader that returns a dict of ObservationPoint(s)
+# {"name": ObservationPoint}
 
 
-class hisreader(object):
-    def __init__(self, inputdir, outputdir):
+class ExtStructure(Structure):
+    Measured: Optional[PandasDataFrame]
+    Simulated: PandasDataFrame
+
+    def default_plot(self, dfs: List, variable: str, labels: List = None) -> None:
+        plt.figure(figsize=(12,4))
+        for ix, df in enumerate(dfs):
+            if labels is None:
+                plt.plot(df[variable].dropna())
+            else:
+                plt.plot(df[variable].dropna(), label=labels[ix])
+
+        if labels is not None:
+            plt.legend()
+
+        plt.gca().update(
+            dict(title=r"Plot of: " + variable, xlabel="date", ylabel=variable)
+        )
+        plt.grid()
+        plt.show()
+
+    def simulated_plot(self, variable: str) -> None:
+        self.default_plot(dfs=[self.Simulated], variable=variable)
+
+    def measured_plot(self, variable: str) -> None:
+        self.default_plot(dfs=[self.Measured], variable=variable)
+
+    def measured_vs_simulated_plot(self, variable: str) -> None:
+        self.default_plot(
+            dfs=[self.Simulated, self.Measured],
+            variable=variable,
+            labels=["Simulated " + variable, "Measured " + variable],
+        )
+
+
+class ObservationPoint(ExtStructure):
+    """Data model for observation points"""
+
+    type: Literal["observation point"] = Field("observation point")
+
+
+class Weir(Structure):
+    """Data model for weirs"""
+
+    type: Literal["weir"] = Field("weir")
+
+
+## Define new structures here
+
+
+class HisResults(object):
+    def __init__(self, inputdir: str, outputdir: str) -> None:
+        # TODO: let user choose what to execute
         self.__inputdir = inputdir
         self.__outputdir = outputdir
-        self.__read_ncvar()
-        self.variables = self.__ds.variables
-        self.obs_names = np.array(
-            nc.chartostring(self.__ds.variables["station_name"][:, :])
-        )
-        self.weir_names = np.array(
-            nc.chartostring(self.__ds.variables["weirgen_id"][:, :])
-        )
+        self.__read_netcdf()
+        self.__read_netcdf_variables()
+        self.__make_timeframe()
+        self.__parse_observation_points()
+        self.__parse_weirs()
+        del self.__ds
 
-    def __read_ncvar(self):
+    def __read_netcdf(self) -> None:
         fnin = list(pathlib.Path(self.__inputdir).glob("*his.nc"))
         self.__ds = uf.DatasetUG(fnin[0], "r")
+        self.variables = self.__ds.variables
 
-        # resultaten uitlezen
-        self.__ds.variables.keys()
-        columns = [
-            "dtype",
-            "standard_name",
-            "long_name",
-            "units",
-            "shape",
-            "coordinates",
-        ]
-        variables = pd.DataFrame(columns=columns)
-        for var in self.__ds.variables.keys():
-            vardata = self.__ds.variables[var]
-            variables.loc[var] = [
-                getattr(vardata, col) if hasattr(vardata, col) else ""
-                for col in columns
+    def __read_netcdf_variables(self) -> None:
+        for key in self.__ds.variables.keys():
+            if "_id" in key:
+                setattr(
+                    self, key, np.array(nc.chartostring(self.__ds.variables[key][:, :]))
+                )
+
+            else:
+                setattr(self, key, self.__ds.variables[key])
+
+    def __make_timeframe(self) -> None:
+        t_start = (
+            self.__ds.variables["time"]
+            .units.split("since ")[1]
+            .replace("+00:00", "")
+            .strip()
+        )
+        start_datetime = dt.datetime.fromisoformat(t_start)
+        end_datetime = start_datetime + dt.timedelta(
+            seconds=np.amax(self.__ds.variables["time"][:])
+        )
+        delta_time = self.__ds.variables["time"][1] - self.__ds.variables["time"][0]
+        df = (
+            pd.date_range(
+                start_datetime,
+                end_datetime,
+                freq="{}S".format(delta_time),
+            )
+            .to_frame(name="time")
+            .set_index("time")
+        )
+        self.timeframe = df
+
+    def __parse_structures(
+        self, structure_type: str, structure_obj: Type[Structure], variables: List
+    ) -> List:
+        """General function to parse structures from His files"""
+        structure_list = []
+        structure_names = np.array(
+            nc.chartostring(self.__ds.variables[structure_type + r"_id"][:, :])
+        )
+        for struct_ix in range(len(structure_names)):
+            data = self.timeframe.copy()
+            for variable in variables:
+                data[variable] = self.__ds.variables[variable][:, struct_ix]
+
+            name = structure_names[struct_ix]
+            xcoordinate = self.__ds.variables[structure_type + r"_geom_node_coordx"][
+                struct_ix
+            ]
+            ycoordinate = self.__ds.variables[structure_type + r"_geom_node_coordy"][
+                struct_ix
             ]
 
-        # tijd toevoegen (obv relatieve tijd ten opzichte van startdatum)
-        time_var = self.__ds.variables["time"]
-        t = dt.datetime.strptime(
-            time_var.units.split("since ")[1].replace("+00:00", "").strip(),
-            "%Y-%m-%d %H:%M:%S",
-        )
-        time = self.__ds.variables["time"][:]
-        time2 = np.empty(time.size)
-        t2 = []
-        for i in range(0, len(time)):
-            t2.append((t + dt.timedelta(0, time[i])).strftime("%d-%m-%Y %H:%M"))
-        time2 = pd.DataFrame(t2)
-        df = time2
-        df.columns = ["time"]
-        self.__df = df
-
-        #%% toevoegen data in csv
-
-    def writeobsstations(
-        self, variables=["waterlevel", "waterdepth", "discharge_magnitude"]
-    ):
-        """
-        Extracts and restructures the water level at model defined specified
-        observation station locations
-        extracts from self.__ds.variables in self.hisreader
-        extracts a pandas dataframe with water levels at the appropriate time
-        and location to self.__df[obs_names] in self.hisreader
-
-        Parameters
-        ----------
-        variables : list
-            A list of strings with variable_names
-
-        Returns
-        -------
-        None.
-
-        """
-        if "station_name" in self.__ds.variables:
-            obs_name = nc.chartostring(self.__ds.variables["station_name"][:, :])
-            obs_names = np.array(obs_name)
-            obs_loc = range(0, len(obs_name))
-            for variable in variables:
-                variable_ds = self.__ds.variables[variable][:, :]
-                self.__df[
-                    [f"{obs.strip()}_{variable}" for obs in obs_names]
-                ] = pd.DataFrame(variable_ds[:, obs_loc])
-
-    def writeweirs(
-        self, variables=["weirgen_crest_level", "weirgen_s1up", "weirgen_s1dn"]
-    ):
-        """
-        Extracts and restructures the water level at model defined specified
-        observation station locations
-        extracts from self.__ds.variables in self.hisreader
-        extracts a pandas dataframe with water levels at the appropriate time
-        and location to self.__df[obs_names] in self.hisreader
-
-        Parameters
-        ----------
-        variables : list
-            A list of strings with variable_names
-
-        Returns
-        -------
-        None.
-
-        """
-        if "weirgen_id" in self.__ds.variables:
-            obs_name = nc.chartostring(self.__ds.variables["weirgen_id"][:, :])
-            obs_names = np.array(obs_name)
-            obs_loc = range(0, len(obs_name))
-            for variable in variables:
-                variable_ds = self.__ds.variables[variable][:, :]
-                self.__df[
-                    [f"{obs.strip()}_{variable}" for obs in obs_names]
-                ] = pd.DataFrame(variable_ds[:, obs_loc])
-
-    def writecrosssections(self):
-        """
-        Extracts and restructures the water level at model defined specified
-        cross section locations
-        extracts from self.__ds.variables in self.hisreader
-        extracts a pandas dataframe with water levels at the appropriate time
-        and location to self.__df[cross_names] in self.hisreader
-
-        Returns
-        -------
-        None.
-
-        """
-        if "cross_section_name" in self.__ds.variables:
-            cross_name = nc.chartostring(
-                self.__ds.variables["cross_section_name"][:, :]
+            structure = structure_obj(
+                id=name,
+                name=name,
+                numcoordinates=2,
+                xcoordinates=[xcoordinate, xcoordinate],
+                ycoordinates=[ycoordinate, ycoordinate],
+                Measured=self.timeframe.copy(),
+                Simulated=data,
             )
-            cross_names = np.array(cross_name)
+            structure_list.append(structure)
+        return structure_list
 
-            q_cross = self.__ds.variables["cross_section_discharge"][:, :]
-            q_cross_section = pd.DataFrame(q_cross)
-            self.__df[cross_names] = q_cross_section
+    def __parse_observation_points(
+        self, variables: List = ["waterlevel", "waterdepth", "discharge_magnitude"]
+    ) -> None:
+        """Specific function to parse observation points from His file"""
+        if "station_id" in self.__ds.variables:
+            self.observation_points = self.__parse_structures(
+                structure_type="station",
+                structure_obj=ObservationPoint,
+                variables=variables,
+            )
 
-    def writedambreaks(self):
-        """
-        Extracts and restructures the water level at model defined specified
-        dam break locations
-        extracts from self.__ds.variables in self.hisreader
-        extracts a pandas dataframe with water levels at the appropriate time
-        and location to self.__df[dambreak_id] in self.hisreader
+    def __parse_weirs(
+        self, variables: List = ["weirgen_crest_level", "weirgen_s1up", "weirgen_s1dn"]
+    ) -> None:
+        """Specific function to parse weirs from His file"""
+        if "weirgen_id" in self.__ds.variables:
+            self.weirs = self.__parse_structures(
+                structure_type="weirgen", structure_obj=Weir, variables=variables
+            )
 
-        Returns
-        -------
-        None.
-
-        """
-        if "dambreak_id" in self.__ds.variables:
-            bres_name = nc.chartostring(self.__ds.variables["dambreak_id"][:, :])
-            bres_names = np.array(bres_name)
-            bres = self.__ds.variables["dambreak_crest_width"][:, :]
-            bres_debiet = self.__ds.variables["dambreak_discharge"][:, :]
-            bres_wl_up = self.__ds.variables["dambreak_s1up"][:, :]
-            bres_wl_dn = self.__ds.variables["dambreak_s1dn"][:, :]
-
-            for index, breach_location in enumerate(bres_names):
-                self.__df[f"bresbreedte_{breach_location}"] = bres[:, index]
-                self.__df[f"bresdebiet_{breach_location}"] = bres_debiet[:, index]
-                self.__df[f"bres_wl_up_{breach_location}"] = bres_wl_up[:, index]
-                self.__df[f"bres_wl_dn_{breach_location}"] = bres_wl_dn[:, index]
-
-    def writeall(self):
-        """
-        Extracts the water level at all currently available variables,
-        being the dambreak, crosssection and observation station locations.
-        extracts from self.__ds.variables in self.hisreader
-        extracts a pandas dataframe with water levels at the appropriate time
-        and location to self.__df[dambreak_id] in self.hisreader
-
-        Returns
-        -------
-        None.
-
-        """
-        if "station_name" in self.__ds.variables:
-            self.writeobsstations()
-        if "cross_section_name" in self.__ds.variables:
-            self.writecrosssections()
-        if "dambreak_id" in self.__ds.variables:
-            self.writedambreaks()
-
-    def writehisdata(self):
-        """
-        function that creates a csv file and writes the extracted
-        parameters in this file
-
-        Returns
-        -------
-        None.
-
-        """
-        self.__df.to_csv(os.path.join(self.__outputdir, "hisdata.csv"), index=False)
+    # Define new structure parsers here
