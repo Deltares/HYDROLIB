f76142775aaae80b7b4f40386f5bafbf9499464c
grongen <guusrongen@gmail.com>
2022-05-16 10:20:52 +0200

    Update test_mesh.py
    Added refinement to tests.
    Added tests for:
    - clipping inside multipolygon
    - clopping outside holed Polygon

--- tests/dhydamo/geometry/test_mesh.py
+++ tests/dhydamo/geometry/test_mesh.py
@@ -1,13 +1,15 @@
 from turtle import circle
 import pytest
 from meshkernel.py_structures import DeleteMeshOption
-from shapely.geometry import box, Polygon, MultiPolygon
+from shapely.geometry import box, Polygon, MultiPolygon, LineString
 
 from hydrolib.core.io.mdu.models import FMModel
 from hydrolib.dhydamo.geometry import mesh, viz
 import numpy as np
 
 import matplotlib.pyplot as plt
+
+from hydrolib.dhydamo.geometry.models import GeometryList
 
 
 @pytest.mark.plots
@@ -41,9 +43,13 @@ def test_create_2d_rectilinear():
     # viz.plot_network(network)
 
 
-def _get_circle_polygon(radius, n_points=361):
+def _get_circle_polygon(
+    radius, n_points: int = 361, xoff: float = 0, yoff: float = 0
+) -> Polygon:
     theta = np.linspace(0, 2 * np.pi, n_points)
     coords = np.c_[np.sin(theta), np.cos(theta)] * radius
+    coords[:, 0] += xoff
+    coords[:, 1] += yoff
     circle = Polygon(coords)
     return circle
 
@@ -67,10 +73,10 @@ def test_create_2d_rectilinear_within_circle():
     )
 
     # Plot to verify
-    # fig, ax = plt.subplots()
-    # viz.plot_network(network, ax=ax)
-    # ax.plot(*circle.exterior.coords.xy, color="red", ls="--")
-    # plt.show()
+    fig, ax = plt.subplots()
+    viz.plot_network(network, ax=ax)
+    ax.plot(*circle.exterior.coords.xy, color="red", ls="--")
+    plt.show()
 
     # Test if 80 (of the 100) cells are left
     assert len(network._mesh2d.mesh2d_face_x) == 80
@@ -251,11 +257,10 @@ def test_create_2d_rectangular_from_multipolygon():
     fmmodel = FMModel()
     network = fmmodel.geometry.netfile.network
 
+    # Create 2 part mesh
     polygon1 = box(0, 0, 10, 10)
     polygon2 = box(12, 2, 19, 9)
-    river = box(-1, 5, 21, 6.5)
-
-    multipolygon = MultiPolygon([polygon1, polygon2]).difference(river)
+    multipolygon = MultiPolygon([polygon1, polygon2])
 
     mesh.add_2dmesh_rectilinear(
         network,
@@ -264,13 +269,23 @@ def test_create_2d_rectangular_from_multipolygon():
         dy=1,
         deletemeshoption=DeleteMeshOption.ALL_FACE_CIRCUMCENTERS,
     )
+    assert len(network._mesh2d.mesh2d_face_x) == 149
 
-    # Check bounds and number of faces
-    assert network._mesh2d.mesh2d_node_x.min() == 0.0
-    assert network._mesh2d.mesh2d_node_x.max() == 19.0
-    assert network._mesh2d.mesh2d_node_y.min() == 0.0
-    assert network._mesh2d.mesh2d_node_y.max() == 9.5
-    assert len(network._mesh2d.mesh2d_face_x) == 115
+    x = np.linspace(0, 20, 101)
+    river = LineString(np.c_[x, np.sin(x / 3) + 5]).buffer(0.5)
+
+    # Refine along river
+    refinement = river.buffer(1)
+    mesh.mesh2d_refine(network, refinement, level=1)
+    assert len(network._mesh2d.mesh2d_face_x) == 411
+
+    # Clip river
+    mesh.mesh2d_clip_and_clean(
+        network=network,
+        geometrylist=GeometryList.from_geometry(river),
+        deletemeshoption=DeleteMeshOption.ALL_NODES,
+    )
+    assert len(network._mesh2d.mesh2d_face_x) == 303
 
     # Plot to verify
     fig, ax = plt.subplots()
@@ -279,33 +294,97 @@ def test_create_2d_rectangular_from_multipolygon():
     ax.plot(*polygon1.exterior.coords.xy, color="k", ls="--")
     ax.plot(*polygon2.exterior.coords.xy, color="k", ls="--")
     ax.plot(*river.exterior.coords.xy, color="r", ls="--")
+    ax.plot(*refinement.exterior.coords.xy, color="g", ls="--")
     plt.show()
 
 
 @pytest.mark.plots
-def test_create_2d_rectangular_triangular_mixed():
+def test_create_2d_triangular_from_multipolygon():
 
     # Define polygon
     fmmodel = FMModel()
     network = fmmodel.geometry.netfile.network
 
-    circle = _get_circle_polygon(radius=10)
-    rectangle = box(12, 2, 19, 9)
+    circle1 = _get_circle_polygon(radius=10)
+    circle2 = _get_circle_polygon(radius=7, xoff=20, yoff=2)
+    refinement_box = box(0, 0, 16, 6)
 
-    mesh.add_2dmesh_triangular(network, circle, edge_length=1)
+    multipolygon = MultiPolygon([circle1, circle2])
 
-    mesh.add_2dmesh_rectilinear(
-        network,
-        rectangle,
-        dx=1,
-        dy=1,
-        deletemeshoption=DeleteMeshOption.ALL_FACE_CIRCUMCENTERS,
-    )
+    mesh.add_2dmesh_triangular(network, multipolygon, edge_length=2)
+
+    # Check bounds and number of faces
+    assert len(network._mesh2d.mesh2d_face_x) == 352
+    assert len(network._mesh2d.mesh2d_edge_x) == 553
+
+    # Refine mesh
+    mesh.mesh2d_refine(network, refinement_box, level=1)
+
+    # Check bounds and number of faces
+    assert len(network._mesh2d.mesh2d_face_x) == 636
+    assert len(network._mesh2d.mesh2d_edge_x) == 984
 
     # Plot to verify
     fig, ax = plt.subplots()
     ax.set_aspect(1.0)
     viz.plot_network(network, ax=ax)
-    ax.plot(*circle.exterior.coords.xy, color="k", ls="--")
-    ax.plot(*rectangle.exterior.coords.xy, color="k", ls="--")
+    ax.plot(*circle1.exterior.coords.xy, color="k", ls="--")
+    ax.plot(*circle2.exterior.coords.xy, color="k", ls="--")
+    ax.plot(*refinement_box.exterior.coords.xy, color="g", ls="--")
+    plt.show()
+
+
+@pytest.mark.plots
+def test_2d_clip_outside_polygon():
+
+    # Define polygon
+    fmmodel = FMModel()
+    network = fmmodel.geometry.netfile.network
+
+    rectangle = box(-10, -10, 10, 10)
+
+    dmo = DeleteMeshOption.ALL_FACE_CIRCUMCENTERS
+    mesh.add_2dmesh_rectilinear(network, rectangle, dx=1, dy=1, deletemeshoption=dmo)
+
+    clipgeo = box(-8, -8, 8, 8).difference(
+        MultiPolygon([box(-6, -1, -4, 2), box(4, 5, 7, 7)])
+    )
+
+    mesh.mesh2d_clip_and_clean(network, clipgeo, deletemeshoption=1, inside=False)
+
+    assert len(network._mesh2d.mesh2d_node_x) == 285
+
+    # Plot to verify
+    fig, ax = plt.subplots()
+    ax.set_aspect(1.0)
+    viz.plot_network(network, ax=ax)
+    ax.plot(*clipgeo.exterior.coords.xy, color="k", ls="--")
+    for hole in clipgeo.interiors:
+        ax.plot(*hole.coords.xy, color="r", ls="--")
+    plt.show()
+
+
+def test_2d_clip_inside_multipolygon():
+
+    # Define polygon
+    fmmodel = FMModel()
+    network = fmmodel.geometry.netfile.network
+
+    rectangle = box(-10, -10, 10, 10)
+
+    dmo = DeleteMeshOption.ALL_FACE_CIRCUMCENTERS
+    mesh.add_2dmesh_rectilinear(network, rectangle, dx=1, dy=1, deletemeshoption=dmo)
+
+    clipgeo = MultiPolygon([box(-6, -1, -4, 2), box(4, 5, 7.2, 7.2)])
+
+    mesh.mesh2d_clip_and_clean(network, clipgeo, deletemeshoption=1, inside=True)
+    assert len(network._mesh2d.mesh2d_node_x) == 437
+
+    # Plot to verify
+    fig, ax = plt.subplots()
+
+    ax.set_aspect(1.0)
+    viz.plot_network(network, ax=ax)
+    for polygon in clipgeo.geoms:
+        ax.plot(*polygon.exterior.coords.xy, color="r", ls="--")
     plt.show()
