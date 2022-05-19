ce878f7d0bb3fa36277e3d7571cdabd498f75998
LisaWeijers <lisa.weijers@rhdhv.com>
2022-05-19 14:29:46 +0200

    reset profile optimizer branch

--- contrib/profile-optimizer/hydrolib_ini.py
+++
@@ -1,6 +0,0 @@
-from hydrolib.core.io.ini import io_models, parser
-
-original = 'src/roughness-Channels.ini'
-a = parser(original)
-
-print(a)
--- tests/test_hydrolib.py
+++
@@ -1,5 +0,0 @@
-from _hydrolib import __version__
-
-
-def test_version():
-    assert __version__ == "0.1.0"
