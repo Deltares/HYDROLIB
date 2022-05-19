ca1236f96a48432013a33962c37f8a26801a9dfe
grongen <guusrongen@gmail.com>
2022-05-19 10:04:42 +0200

    Refactored GeometryList model
    Fixed a bug, simplified the code.

--- hydrolib/dhydamo/geometry/models.py
+++ hydrolib/dhydamo/geometry/models.py
@@ -1,7 +1,14 @@
 from meshkernel import GeometryList as GeometryListMK
-from shapely.geometry import Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon
+from shapely.geometry import (
+    Point,
+    MultiPoint,
+    LineString,
+    MultiLineString,
+    Polygon,
+    MultiPolygon,
+)
 import numpy as np
-from typing import Union
+from typing import Union, List
 from hydrolib.core.io.net.models import split_by
 
 
@@ -55,7 +62,9 @@ class GeometryList(GeometryListMK):
             x_crds.append(x_int)
             y_crds.append([cls.inner_outer_separator])
             y_crds.append(y_int)
-        gl = cls(x_coordinates=np.concatenate(x_crds), y_coordinates=np.concatenate(y_crds))
+        gl = cls(
+            x_coordinates=np.concatenate(x_crds), y_coordinates=np.concatenate(y_crds)
+        )
         return gl
 
     @classmethod
@@ -73,7 +82,9 @@ class GeometryList(GeometryListMK):
             x_crds.append(gl.x_coordinates)
             y_crds.append(gl.y_coordinates)
 
-        gl = cls(x_coordinates=np.concatenate(x_crds), y_coordinates=np.concatenate(y_crds))
+        gl = cls(
+            x_coordinates=np.concatenate(x_crds), y_coordinates=np.concatenate(y_crds)
+        )
         return gl
 
     @classmethod
@@ -88,37 +99,66 @@ class GeometryList(GeometryListMK):
     def from_multipoint(cls, multipoint: Point):
         return cls._from_multigeometry(multipoint)
 
-    def to_geometry(self):
-        geometries = [geo for geo in split_by(self, self.geometry_separator) if geo.x_coordinates.size > 0]
-        is_multi = len(geometries) > 1
+    def _to_polygon(
+        self, geometries: List[GeometryListMK], is_multi: bool
+    ) -> Union[Polygon, MultiPolygon]:
+        polygons = []
+        for geometry in geometries:
+            parts = [
+                np.stack([p.x_coordinates, p.y_coordinates], axis=1)
+                for p in split_by(geometry, self.inner_outer_separator)
+            ]
+            polygons.append(Polygon(shell=parts[0], holes=parts[1:]))
+        if is_multi:
+            return MultiPolygon(polygons)
+        else:
+            return polygons[0]
 
-        for geometry_list in geometries:
-            is_polygon = geometry_list.x_coordinates[0] == geometry_list.x_coordinates[-1]
-            is_linestring = (len(geometry_list.x_coordinates) > 1) and (not is_polygon)
-
-            if is_polygon:
-                polygons = []
-                for geometry in geometries:
-                    parts = [
-                        np.stack([p.x_coordinates, p.y_coordinates], axis=1)
-                        for p in split_by(geometry, self.inner_outer_separator)
-                    ]
-                    polygons.append(Polygon(shell=parts[0], holes=parts[1:]))
-                if is_multi:
-                    return MultiPolygon(polygons)
-                else:
-                    return polygons[0]
-
-            elif is_linestring:
-                linestrings = [LineString(np.stack([p.x_coordinates, p.y_coordinates], axis=1)) for p in geometries]
+    def _to_linestring(
+        self, geometries: List[GeometryListMK], is_multi: bool
+    ) -> Union[LineString, MultiLineString]:
+                linestrings = [
+                    LineString(np.stack([p.x_coordinates, p.y_coordinates], axis=1))
+                    for p in geometries
+                ]
                 if is_multi:
                     return MultiLineString(linestrings)
                 else:
                     return linestrings[0]
 
+    def _to_points(
+        	self, geometries: List[GeometryListMK], is_multi: bool
+    ) -> Union[Point, MultiPoint]:
+        points = [
+            Point(np.stack([p.x_coordinates, p.y_coordinates], axis=1))
+            for p in geometries
+        ]
+        if is_multi:
+            return MultiPoint(points)
+        else:
+            return points[0]
+
+
+    def to_geometry(self):
+        geometries = [
+            geo
+            for geo in split_by(self, self.geometry_separator)
+            if geo.x_coordinates.size > 0
+        ]
+        is_multi = len(geometries) > 1
+
+        for geometry_list in geometries:
+            is_polygon = (
+                geometry_list.x_coordinates[0] == geometry_list.x_coordinates[-1]
+            )
+            is_linestring = (len(geometry_list.x_coordinates) > 1) and (not is_polygon)
+
+            if is_polygon:
+                return self._to_polygon(geometries, is_multi)
+
+            elif is_linestring:
+                return self._to_linestring(geometries, is_multi)
+
             else:
-                points = [Point(np.stack([p.x_coordinates, p.y_coordinates], axis=1)) for p in geometries]
-                if is_multi:
-                    return MultiPoint(linestrings)
-                else:
-                    return points[0]
+                return self._to_point(geometries, is_multi)
+                
