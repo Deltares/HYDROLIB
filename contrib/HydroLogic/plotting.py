1f4778a24266e8479af2b832dbc9a729d2b7ea25
Kreef <31723681+Kreef@users.noreply.github.com>
2022-05-25 10:30:20 +0200

    Add files via upload

---
+++ contrib/HydroLogic/plotting.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from typing import List
+
+def default_plot(dfs: List, variable: str, labels: List = None) -> None:
+    """Basic plot function that takes a number of DataFrames and plots the 'variable' columns in one figure"""
+    plt.figure(figsize=(12, 4))
+    for ix, df in enumerate(dfs):
+        if labels is None:
+            plt.plot(df[variable].dropna())
+        else:
+            plt.plot(df[variable].dropna(), label=labels[ix])
+
+    if labels is not None:
+        plt.legend()
+
+    plt.gca().update(
+        dict(title=r"Plot of: " + variable, xlabel="date", ylabel=variable)
+    )
+    plt.grid()
+    plt.show()
