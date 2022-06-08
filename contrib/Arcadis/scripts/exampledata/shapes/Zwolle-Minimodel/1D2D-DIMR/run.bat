ae1d8b604a5983c154d15582978dd5058b74e541
Robbertdelange <robbert.delange@arcadis.com>
2022-06-08 13:38:24 +0200

    Voorbeeld zwolle mini-model

---
+++ contrib/Arcadis/scripts/exampledata/shapes/Zwolle-Minimodel/1D2D-DIMR/run.bat
@@ -0,0 +1,7 @@
+@ echo off
+set OMP_NUM_THREADS=1
+
+call "C:\Program Files\Deltares\D-HYDRO Suite 1D2D (1.0.0.53506)\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat"
+
+    rem To prevent the DOS box from disappearing immediately: remove the rem on the following line
+pause
