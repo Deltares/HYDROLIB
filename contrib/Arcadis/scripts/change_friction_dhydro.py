007f90f2b287d87b5a36e1e88b860ad62b5919f6
Robbertdelange <robbert.delange@arcadis.com>
2022-07-20 13:28:00 +0200

    Made a somewhat better coded function for changing the friction.

--- contrib/Arcadis/scripts/change_friction_dhydro.py
+++ contrib/Arcadis/scripts/change_friction_dhydro.py
@@ -8,52 +8,15 @@ import pandas as pd
 from hydrolib.core.io.mdu.models import FMModel, FrictionModel
 
 # nodig voor inladen ander script
-currentdir = os.path.dirname(os.path.abspath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
+# currentdir = os.path.dirname(os.path.abspath(__file__))
+# parentdir = os.path.dirname(currentdir)
+# sys.path.insert(0, parentdir)
 from read_dhydro import net_nc2gdf
 
-
-def read_frictionfiles(fm):
-    dfsglob = {}
-    dfschan = {}
-
-    # loop through friction files and store them in separate dictionaries
-    for i in range(len(fm.geometry.frictfile)):
-        dfglob = pd.DataFrame([f.__dict__ for f in fm.geometry.frictfile[i].global_])
-        dfsglob[str(str(fm.geometry.frictfile[i].global_[0].frictionid))] = dfglob
-
-        dfchan = pd.DataFrame([f.__dict__ for f in fm.geometry.frictfile[i].branch])
-        dfschan[str(str(fm.geometry.frictfile[i].global_[0].frictionid))] = dfchan
-    return dfsglob, dfschan
-
-
-def read_crossections(fm):
-
-    dfcrsdef = pd.DataFrame([f.__dict__ for f in fm.geometry.crossdeffile.definition])
-    dfcrsloc = pd.DataFrame([f.__dict__ for f in fm.geometry.crosslocfile.crosssection])
-
-    return dfcrsdef, dfcrsloc
-
-
-def read_shape(gispath, frictcolumn):
-    areas_selection = gpd.read_file(gispath)
-    friction = areas_selection[str(frictcolumn)]
-    print()
-
-
-if __name__ == "__main__":
-    # Read shape
-    shape_path = r"C:\Users\delanger3781\OneDrive - ARCADIS\Documents\DHydro\Zwolle-Minimodel\Zwolle-Minimodel\frictfiles.shp"
-    output_location = (
-        r"C:\scripts\AHT_scriptjes\Hydrolib\Dhydro_changefrict\roughness-Section000.ini"
-    )
-    input_mdu = Path(
-        r"C:\scripts\AHT_scriptjes\Hydrolib\Dhydro_changefrict\data\Zwolle-Minimodel\1D2D-DIMR\dflowfm\flowFM.mdu"
-    )
+def change_friction_shape(input_mdu,shape_path,output_path):
     fm = FMModel(input_mdu)
 
-    dict_global, dict_chann = read_frictionfiles(fm)
+    df_glob, df_chan = read_frictionfiles(fm)
 
     gdf_frict = gpd.read_file(shape_path)
     gdf_frict_buf = gpd.GeoDataFrame(gdf_frict, geometry=gdf_frict.buffer(1))
@@ -70,53 +33,46 @@ if __name__ == "__main__":
 
     # TODO: Filteren op branchtype (wordt nu nog pipe meegenomen)
     # TODO: Hoe halve watergangen verwerken
+   
+    for file in df_chan:
+        if not df_chan[file].empty:
+            for index, row in df_chan[file].iterrows():
+                branchid = row["branchid"]
+                
+                # TODO: Als er twee waardes zijn in de intersect, wordt de laatste gepakt. Dat is nog incorrect
+                if branchid in (list(intersect.id)):
+                    df_chan[file].at[df_chan[file].loc[df_chan[file]["branchid"] == branchid,"frictionvalues"].index.tolist()[0],"frictionvalues"] = [intersect.loc[intersect["id"] == branchid,"fricval"].iloc[0]]*int(df_chan[file].at[df_chan[file].loc[df_chan[file]["branchid"] == branchid,"numlocations"].index.tolist()[0],"numlocations"])
+            
+            writefile = FrictionModel(global_=df_glob[file].to_dict("records"), branch = df_chan[file].to_dict("records"))
+            writefile.save(Path(output_path) / (str("roughness-"+file + ".ini")))
+        else:
+            writefile = FrictionModel(global_=df_glob[file].to_dict("records"))
+            writefile.save(Path(output_path) / (str("roughness-"+file + ".ini")))
+            
+def read_frictionfiles(fm):
+    dfsglob = {}
+    dfschan = {}
 
-    for file in dict_chann:
-        if not dict_chann[file].empty:
-            for id in dict_chann[file].branchid:
-                if id in intersect["id"].values:
-                    print(id)
-                    # TODO: Als er twee waardes, wordt de laatste gepakt. Dat is nog incorrect
-                    # dict_chann[file].loc[dict_chann[file].branchid == id, 'frictionvalues'] = list(intersect.loc[intersect["id"] == id, "fricval"])
-                    dict_chann[file].at[
-                        dict_chann[file]
-                        .loc[dict_chann[file].branchid == id]
-                        .index.to_list()[0],
-                        "frictionvalues",
-                    ] = list(intersect.loc[intersect["id"] == id, "fricval"])
-    for file in dict_chann:
-        writer = FrictionModel(branch=dict_chann[file].to_dict("records"))
+    # loop through friction files and store them in separate dictionaries
+    for i in range(len(fm.geometry.frictfile)):
+        dfglob = pd.DataFrame([f.__dict__ for f in fm.geometry.frictfile[i].global_])
+        dfsglob[str(str(fm.geometry.frictfile[i].global_[0].frictionid))] = dfglob
+               
+        dfchan = pd.DataFrame([f.__dict__ for f in fm.geometry.frictfile[i].branch])
+        dfschan[str(str(fm.geometry.frictfile[i].global_[0].frictionid))] = dfchan
 
-    # wegschrijven van .ini file met nieuwe fricties
-    # TODO: losse ini wegschrijven er vanuit gaande dat hij in "section000" staat. Nieuwe wegschrijven
-    # anders: nieuwe crsdef nodig.
+    return dfsglob, dfschan
 
-    global_value = 31
-    frict_id = str("FloodPlain1")
 
-    # TODO hydrolib core writer gebruiker (.save)
-    with open(output_location, "w") as f:
-        f.write(
-            "[General]\n    fileVersion           = 2.00\n    fileType              = roughness\n"
-        )
-        f.write(
-            "\n[Global]\n    frictionId            = "
-            + frict_id
-            + "\n    frictionType          = Strickler\n    frictionValue         = "
-            + str(global_value)
-            + "\n"
-        )
+if __name__ == "__main__":
+    # Read shape
+    shape_path = r"C:\Users\delanger3781\OneDrive - ARCADIS\Documents\DHydro\Zwolle-Minimodel\Zwolle-Minimodel\frictfiles.shp"
+    output_path = (
+        r"C:\TEMP\AHT_test_output"
+    )
+    input_mdu = Path(
+        r"C:\scripts\AHT_scriptjes\Hydrolib\Dhydro_changefrict\data\Zwolle-Minimodel\1D2D-DIMR\dflowfm\flowFM.mdu"
+    )
+    change_friction_shape(input_mdu,shape_path,output_path)
+    
 
-        for id in intersect["id"]:
-            f.write("\n[Branch]\n    branchId              = " + id + "\n")
-            f.write("    frictionType          = " + "Strickler" + "\n")
-            f.write("    functionType          = " + "Constant" + "\n")
-            f.write("    numLocations          = " + "1" + "\n")
-            f.write("    chainage              = " + "0.000000" + "\n")
-            f.write(
-                "    frictionValues        = "
-                + str(intersect.loc[intersect["id"] == id, "fricval"].values[0])
-                + "\n"
-            )
-
-    print("done")
