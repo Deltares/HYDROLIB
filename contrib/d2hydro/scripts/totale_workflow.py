102bd53c2ef55508b08bf7b243074072a605f2a2
d2hydro <d2hydro@users.noreply.github.com>
2022-08-29 09:38:36 +0000

    autoformat: isort & black

--- contrib/d2hydro/scripts/totale_workflow.py
+++ contrib/d2hydro/scripts/totale_workflow.py
@@ -1,6 +1,6 @@
-from cmt.project import Project
 from pathlib import Path
 
+from cmt.project import Project
 
 stochastics_json = Path(r"../data/dellen/populate_cases.json")
 project = Project(filepath=r"../data/stochast").from_stochastics(stochastics_json)
